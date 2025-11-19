d99701533f6091249668c13c1ea44cd759455c5b
lanOAO <qiulannana@163.com>
2025-11-19 19:09:09 +0800

    Site updated: 2025-11-19 19:09:08

--- js/hexo_resize_image.js
+++ js/hexo_resize_image.js
@@ -1,28 +1,37 @@
-function hexo_resize_image() {
+function set_image_size(image, width, height) 
+{
+    image.setAttribute("width", width + "px");
+    image.setAttribute("height", height + "px");
+}
+
+function hexo_resize_image()
+{
     var imgs = document.getElementsByTagName('img');
-    for (var i = imgs.length - 1; i >= 0; i--) {
+    for (var i = imgs.length - 1; i >= 0; i--) 
+    {
         var img = imgs[i];
-        var src = img.getAttribute('src');
 
-        // 如果没有 src，跳过这个 img
-        if (!src) continue;
-
-        src = src.toString();
+        var src = img.getAttribute('src').toString();
 
         var fields = src.match(/(?<=\?)\d*x\d*/g);
-        if (fields && fields.length == 1) {
+        if (fields && fields.length == 1)
+        {
             var values = fields[0].split("x");
-            if (values.length == 2) {
+            if (values.length == 2)
+            {
                 var width = values[0];
                 var height = values[1];
 
-                if (!(width.length && height.length)) {
+                if (!(width.length && height.length))
+                {
                     var n_width = img.naturalWidth;
                     var n_height = img.naturalHeight;
-                    if (width.length > 0) {
+                    if (width.length > 0)
+                    {
                         height = n_height*width/n_width;
                     }
-                    if (height.length > 0) {
+                    if (height.length > 0)
+                    {
                         width = n_width*height/n_height;
                     }
                 }
@@ -32,7 +41,8 @@ function hexo_resize_image() {
         }
 
         fields = src.match(/(?<=\?)\d*/g);
-        if (fields && fields.length == 1) {
+        if (fields && fields.length == 1)
+        {
             var scale = parseFloat(fields[0].toString());
             var width = scale/100.0*img.naturalWidth;
             var height = scale/100.0*img.naturalHeight;
@@ -40,3 +50,4 @@ function hexo_resize_image() {
         }
     }
 }
+window.onload = hexo_resize_image;
